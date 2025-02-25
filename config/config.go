84ad5dc6006276061d33ed8ff6dcc42ca82f96bb
Siddarth Vijay <vsiddarthvijay@gmail.com>
2025-02-25 17:16:09 +0530

    chore: logs configs etc

---
+++ config/config.go
@@ -0,0 +1,38 @@
+package config
+
+import (
+	"encoding/json"
+	"log"
+	"os"
+)
+
+type Config struct {
+	AvalancheAPI          string `json:"avalanche_api"`
+	AggregatorURL         string `json:"aggregator_url"`
+	SigningSubnetID       string `json:"signing_subnet_id"`
+	QuorumPercentage      int    `json:"quorum_percentage"`
+	BeamRPC               string `json:"beam_rpc"`
+	StakingManagerAddress string `json:"contract_address"`
+	PrivateKey            string `json:"private_key"`
+	LogLevel              string `json:"log_level"`
+}
+
+func LoadConfig(path string) (*Config, error) {
+	file, err := os.Open(path)
+	if err != nil {
+		return nil, err
+	}
+	defer file.Close()
+
+	decoder := json.NewDecoder(file)
+	cfg := &Config{
+		QuorumPercentage: 67,
+		LogLevel:         "info",
+	}
+	if err := decoder.Decode(cfg); err != nil {
+		return nil, err
+	}
+
+	log.Printf("Loaded configuration from %s", path)
+	return cfg, nil
+}
